f1f2b2a547b0d857140de9eb3afec9a52f545be3
rickendrock-ui <rickendrock@users.noreply.github.com>
2026-08-09 02:26:04 +0800

    update cepat

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ubah menjadi '/' agar sesuai dengan standar hosting production
+  base: './', // Ubah menjadi '/' agar sesuai dengan standar hosting production
 })
